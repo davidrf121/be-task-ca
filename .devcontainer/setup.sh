e379b15324e2a7dc52dbf5b08ee90430e7f1f016
David Ruiz Falco <david.rf97@gmail.com>
2025-05-11 20:24:19 +0000

    remove commented code

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,11 +5,4 @@ apt install -y python3-dev libpq-dev libhdf5-dev graphviz
 
 # Download and install POETRY
 curl -sSL https://install.python-poetry.org | python3 -
-
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-
-# Install python3.11
-# apt install -y software-properties-common
-# add-apt-repository ppa:deadsnakes/ppa -y
-# apt update
-# apt install -y python3.11 python3.11-venv python3.11-dev
